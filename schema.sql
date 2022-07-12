36471e3439414d28680e0348a2a5586ead3ab41c
David Poole <niculbolas@gmail.com>
2022-07-12 09:57:59 -0400

    Added starter chellenge code, and additional csv files from queries.

--- schema.sql
+++ schema.sql
@@ -51,9 +51,30 @@ CREATE TABLE titles (
 -- Creating table for dept_emp
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-    title VARCHAR NOT NULL,
+    dept_no int NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    PRIMARY KEY (emp_no),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+DROP TABLE dept_emp;
+
+-- Creating table for dept_emp
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
